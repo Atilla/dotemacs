15ca8b01c0f9086868ad44fc08aba48fdd88e7c7
Anton Tsigularov <anton@xeneta.com>
2016-06-14 09:04:04 +0200

    Disable insert

--- keybindings.el
+++ keybindings.el
@@ -3,6 +3,7 @@
 ;;; Code:
 (global-set-key [BackSpace] 'backward-delete-char-untabify)
 (global-set-key [delete] 'delete-char)
+(global-set-key [insert] nil)
 (global-set-key "\C-w" 'backward-kill-word)
 (global-set-key "\C-x\C-k" 'kill-region)
 (global-set-key [(control return)] 'hippie-expand)
